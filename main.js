9583d84ffedc520211abc1b0dc002c26116c9102
stinker13333 <150056430+stinker13333@users.noreply.github.com>
2024-01-05 10:05:37 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+if (a === "G" || "g"){
+  a = "https://google.com"
+} else if (a === "google" || "Google") {
+  a = "https://google.com"
+} else if (a === "y" || "Y") {
+  a = "https://youtube.com"
+} else if (a === "youtube" || "Youtube"){
+  a = "https://youtube.com"
+}
